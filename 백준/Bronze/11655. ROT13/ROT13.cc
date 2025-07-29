431eb1ffeadef64f025854aa1e2fdd68cf0320f5
HyeonHoi Kim <hyeonhoi11@naver.com>
2025-07-29 19:01:14 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	string s;
+	getline(cin, s);
+
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] >= 'a' && s[i] <= 'z') {
+			s[i] = (s[i] - 'a' + 13) % 26 + 'a';	
+		}
+		else if (s[i] >= 'A' && s[i] <= 'Z') {
+			s[i] = (s[i] - 'A' + 13) % 26 + 'A';
+		}
+	}
+
+	cout << s << '\n';
+	return 0;
+}
