e725cf2dbaafc962ea7b4f2d527f890234aef136
HyeonHoi Kim <hyeonhoi11@naver.com>
2025-08-12 12:37:11 +0900

    [Bronze II] Title: 단어의 개수, Time: 64 ms, Memory: 5964 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+using namespace std;
+
+int main() {
+	string s;
+	getline(cin, s);
+
+	stringstream ss(s);
+	string word;
+	int cnt = 0;
+
+	while (ss >> word) {
+		cnt++;
+	}
+
+	cout << cnt << '\n';
+	return 0;
+}
